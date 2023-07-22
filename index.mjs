72e1bd4b90287b7c9f4fb22758daf7e25478eaa9
Alfred <105510070+AlfredUFY@users.noreply.github.com>
2023-07-22 03:26:27 +0000

    add getBlueprint function to retrieve specific blueprint

--- index.mjs
+++ index.mjs
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 import { getShops } from './utils/admin/getShops.mjs';
 import { getBlueprints } from './utils/public/getBlueprints.mjs';
+import { getBlueprint } from './utils/public/getBlueprint.mjs';
 
 dotenv.config();
 
 const apiKey = process.env.API_KEY;
 
 const run = async () => {
-    const result = await getBlueprints(apiKey);
+    const result = await getBlueprint(apiKey, 3);
     console.log(result);
 };
 
